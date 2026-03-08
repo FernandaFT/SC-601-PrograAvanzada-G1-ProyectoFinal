bbddad862d0f5972153cdc86a80a82af1eb865a0
Jeancr14 <jsegura30896@ufide.ac.cr>
2026-03-08 17:15:05 -0600

    CRUD de USUARIOS implementado

---
+++ ProyectoFinalG1/Controllers/USUARIOSController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoFinalG1.EntityFramework;
+
+namespace ProyectoFinalG1.Controllers
+{
+    public class USUARIOSController : Controller
+    {
+        private WaggyEntities db = new WaggyEntities();
+
+        // GET: USUARIOS
+        public ActionResult Index()
+        {
+            return View(db.USUARIOS.ToList());
+        }
+
+        // GET: USUARIOS/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            USUARIOS uSUARIOS = db.USUARIOS.Find(id);
+            if (uSUARIOS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uSUARIOS);
+        }
+
+        // GET: USUARIOS/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: USUARIOS/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID_USUARIO,IDENTIFICACION,NOMBRE,CORREO,PASSWORD,ESTADO,ROL")] USUARIOS uSUARIOS)
+        {
+            if (ModelState.IsValid)
+            {
+                db.USUARIOS.Add(uSUARIOS);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(uSUARIOS);
+        }
+
+        // GET: USUARIOS/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            USUARIOS uSUARIOS = db.USUARIOS.Find(id);
+            if (uSUARIOS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uSUARIOS);
+        }
+
+        // POST: USUARIOS/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID_USUARIO,IDENTIFICACION,NOMBRE,CORREO,PASSWORD,ESTADO,ROL")] USUARIOS uSUARIOS)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(uSUARIOS).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(uSUARIOS);
+        }
+
+        // GET: USUARIOS/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            USUARIOS uSUARIOS = db.USUARIOS.Find(id);
+            if (uSUARIOS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uSUARIOS);
+        }
+
+        // POST: USUARIOS/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            USUARIOS uSUARIOS = db.USUARIOS.Find(id);
+            db.USUARIOS.Remove(uSUARIOS);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}

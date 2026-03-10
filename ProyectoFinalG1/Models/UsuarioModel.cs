39a0d016c1084ebb86128686f03db354a735240d
Jeancr14 <jsegura30896@ufide.ac.cr>
2026-03-09 19:43:40 -0600

    Actualización de Models
    -Se Agrego Nuevamente MODELS ya que por error se elimino, ahora el proyecto funciona correctamente

---
+++ ProyectoFinalG1/Models/UsuarioModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ProyectoFinalG1.Models
+{
+    public class UsuarioModel
+    {
+        public long IdUsuario { get; set; }
+
+        public string Identificacion { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Correo { get; set; }
+
+        public string Password { get; set; }
+
+        public string Estado { get; set; }
+
+        public string Rol { get; set; }
+    }
+}

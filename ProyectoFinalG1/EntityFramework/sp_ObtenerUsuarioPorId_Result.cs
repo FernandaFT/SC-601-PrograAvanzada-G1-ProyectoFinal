b89e5e80cf1dcab49a8c93fccd8971bad3ec6449
Aaron Azofeifa <aazofeifa60571@ufide.ac.cr>
2026-03-15 14:30:21 -0600

    Creación de Registro

--- ProyectoFinalG1/EntityFramework/sp_ObtenerUsuarioPorId_Result.cs
+++ ProyectoFinalG1/EntityFramework/sp_ObtenerUsuarioPorId_Result.cs
@@ -10,20 +10,15 @@
 namespace ProyectoFinalG1.EntityFramework
 {
     using System;
-    using System.Collections.Generic;
     
-    public partial class rol
+    public partial class sp_ObtenerUsuarioPorId_Result
     {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
-        public rol()
-        {
-            this.usuario = new HashSet<usuario>();
-        }
-    
         public int consecutivo { get; set; }
-        public string descripcion { get; set; }
-    
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<usuario> usuario { get; set; }
+        public string identificacion { get; set; }
+        public string nombre { get; set; }
+        public string correoElectronico { get; set; }
+        public string telefono { get; set; }
+        public string direccion { get; set; }
+        public int consecutivoRol { get; set; }
     }
 }
--- ProyectoFinalG1/Template/js/funciones/registro.js
+++ ProyectoFinalG1/Template/js/funciones/registro.js
@@ -1,33 +1,62 @@
-﻿$(function () {
+﻿$(document).ready(function () {
     $("#FormRegistro").validate({
         rules: {
             Identificacion: {
-                required: true
+                required: true,
+                minlength: 11
             },
             Nombre: {
                 required: true
             },
-            Correo: {
+            CorreoElectronico: {
                 required: true,
                 email: true
             },
-            Password: {
+            Contrasenna: {
+                required: true,
+                minlength: 6
+            },
+            Telefono: {
+                required: true,
+                digits: true, 
+                minlength: 8
+            },
+            ConsecutivoRol: {
                 required: true
+            },
+            Direccion: {
+                required: true,
+                maxlength: 255
             }
+
         },
         messages: {
             Identificacion: {
-                required: "Campo requerido"
+                required: "Campo requerido",
+                minlength: "Debe tener al menos 11 dígitos"
             },
             Nombre: {
                 required: "Campo requerido"
             },
-            Correo: {
+            CorreoElectronico: {
                 required: "Campo requerido",
                 email: "Formato incorrecto"
             },
-            Password: {
+            Contrasenna: {
+                required: "Campo requerido",
+                minlength: "Debe tener al menos 6 dígitos"
+            },
+            Telefono: {
+                required: "Campo requerido",
+                digits: "Solo números permitidos",
+                minlength: "Debe tener al menos 8 dígitos"
+            },
+            ConsecutivoRol: {
                 required: "Campo requerido"
+            },
+            Direccion: {
+                required: "Campo requerido",
+                maxlength: "Máximo 255 carácteres"
             }
         },
 

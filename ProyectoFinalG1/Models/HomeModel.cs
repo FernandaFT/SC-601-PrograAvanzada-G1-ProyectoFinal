05fcbdd6789fc0870848a09ff0f03b2b1c6fb2bb
Fernanda Fajardo <nanda199784@gmail.com>
2026-03-17 23:16:38 -0600

    Corrección del LP y BTN Actualizar

---
+++ ProyectoFinalG1/Models/HomeModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoFinalG1.Models
+{
+    public class HomeModel
+    {
+        public List<ProductoModel> ProductosAlimentos { get; set; }
+        public List<ProductoModel> ProductosRopa { get; set; }
+        public List<ProductoModel> ProductosAccesorios { get; set; }
+    }
+}

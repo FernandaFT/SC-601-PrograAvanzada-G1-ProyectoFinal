92a75a28fd1fdf397ddd50ffd0e051911300bab6
Fernanda Fajardo <nanda199784@gmail.com>
2026-03-21 01:16:27 -0600

    Gestión de Inventario

---
+++ ProyectoFinalG1/Models/InventarioEncViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProyectoFinalG1.Models
+{
+    public class InventarioEncViewModel
+    {
+        public int ConsInventario { get; set; }
+        public DateTime FecInventario { get; set; }
+        public string Observaciones { get; set; }
+        public string EstadoInv { get; set; }
+    }
+}

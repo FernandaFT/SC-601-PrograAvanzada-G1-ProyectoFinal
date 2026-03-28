d0e79d07c5eca7bab52a4090457805ad6e0567e2
Jeancr14 <jsegura30896@ufide.ac.cr>
2026-03-28 11:35:15 -0600

    Creacion HU09-10
    Se creo Consulta de Existencias y consulta de productos bajo el minimo

---
+++ ProyectoFinalG1/Controllers/ExistenciasController.cs
@@ -0,0 +1,75 @@
+﻿using ProyectoFinalG1.EntityFramework;
+using ProyectoFinalG1.Models;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace ProyectoFinalG1.Controllers
+{
+    public class ExistenciasController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index(int pagina = 1)
+        {
+            int registrosPorPagina = 5;
+
+            using (var context = new WaggyDBEntities())
+            {
+                var resultado = context.sp_ConsultarExistencias(null).ToList();
+
+                var totalRegistros = resultado.Count();
+                var totalPaginas = (int)Math.Ceiling((double)totalRegistros / registrosPorPagina);
+
+                var datos = resultado
+                    .Select(p => new ExistenciasModel
+                    {
+                        ConsecutivoProducto = p.cons_producto,
+                        NombreProducto = p.nombre_producto,
+                        Existencia = p.existencia,
+                        ExistenciaMin = p.existenciamin,
+                        ExistenciaMax = p.existenciamax
+                    })
+                    .Skip((pagina - 1) * registrosPorPagina)
+                    .Take(registrosPorPagina)
+                    .ToList();
+
+                ViewBag.PaginaActual = pagina;
+                ViewBag.TotalPaginas = totalPaginas;
+
+                return View(datos);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Index(bool? BajoMinimo, int pagina = 1)
+        {
+            int registrosPorPagina = 5;
+
+            using (var context = new WaggyDBEntities())
+            {
+                var resultado = context.sp_ConsultarExistencias(BajoMinimo).ToList();
+
+                var totalRegistros = resultado.Count();
+                var totalPaginas = (int)Math.Ceiling((double)totalRegistros / registrosPorPagina);
+
+                var datos = resultado
+                    .Select(p => new ExistenciasModel
+                    {
+                        ConsecutivoProducto = p.cons_producto,
+                        NombreProducto = p.nombre_producto,
+                        Existencia = p.existencia,
+                        ExistenciaMin = p.existenciamin,
+                        ExistenciaMax = p.existenciamax
+                    })
+                    .Skip((pagina - 1) * registrosPorPagina)
+                    .Take(registrosPorPagina)
+                    .ToList();
+
+                ViewBag.PaginaActual = pagina;
+                ViewBag.TotalPaginas = totalPaginas;
+
+                return View(datos);
+            }
+        }
+    }
+}
---
+++ ProyectoFinalG1/Models/ExistenciaModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ProyectoFinalG1.Models
+{
+    public class ExistenciasModel
+    {
+        public int ConsecutivoProducto { get; set; }
+        public string NombreProducto { get; set; }
+        public int Existencia { get; set; }
+        public int ExistenciaMin { get; set; }
+        public int ExistenciaMax { get; set; }
+    }
+}
